6d907d2eb7f252fb6b7223e24ba0ec618e68e18a
Dmitry <dmitrymatio@gmail.com>
2019-11-29 18:00:38 -0800

    script.js renders InstaFrame correctly

--- posts.js
+++ posts.js
@@ -1,6 +1,6 @@
-const posts = [{
+let posts = [{
         id: 1,
-        frame: "gradient1",
+        frame: 1,
         username: "soybeanboy",
         message: "It's ruff out here for a puppy.",
         image_url: "Assets/adorable-animal-animal-photography-animal-portrait-594687.jpg",
@@ -15,7 +15,7 @@ const posts = [{
     },
     {
         id: 2,
-        frame: "gradient2",
+        frame: 11,
         username: "soybeanboy",
         message: "It's ruff out here for a puppy.",
         image_url: "Assets/brown-and-white-short-coated-puppy-1805164.jpg",
@@ -30,7 +30,7 @@ const posts = [{
     },
     {
         id: 3,
-        frame: "gradient11",
+        frame: 10,
         username: "soybeanboy",
         message: "It's ruff out here for a puppy.",
         image_url: "Assets/high-angle-photo-of-a-corgi-looking-upwards-2664417.jpg",
@@ -44,8 +44,8 @@ const posts = [{
         ]
     },
     {
-        id: 2,
-        frame: "gradient10",
+        id: 4,
+        frame: 7,
         username: "soybeanboy",
         message: "It's ruff out here for a puppy.",
         image_url: "Assets/pomeranian-puppy-1619690.jpg",
@@ -59,8 +59,8 @@ const posts = [{
         ]
     },
     {
-        id: 2,
-        frame: "gradient5",
+        id: 5,
+        frame: 5,
         username: "soybeanboy",
         message: "It's ruff out here for a puppy.",
         image_url: "Assets/short-coated-white-dog-on-white-textile-846083.jpg",
@@ -74,3 +74,5 @@ const posts = [{
         ]
     },
 ]
+
+/* export { posts }; */
--- script.js
+++ script.js
@@ -1,141 +1,13 @@
-const createRow = () => {
+/* import { posts } from './posts.mjs/index.mjs'; */
+
+const createRow = (rowId) => {
     const main = document.querySelector("main");
     const sectionRow = document.createElement("section");
+
     sectionRow.classList.add("posts-row");
+    sectionRow.classList.add(`row-${rowId}`);
     main.appendChild(sectionRow);
 }
-
-const createPostElement = (postObj, parent) => {
-    const postStructure = [
-        { "element": "article", "class": "insta-post", "imageContainer": "section.posts-row" },
-        { "element": "div", "class": "article-header", "imageContainer": "article.insta-post" },
-        { "element": "h2", "class": "username", "imageContainer": "div.article-header" },
-        { "element": "figure", "class": "article-photo", "imageContainer": "div.article-header" },
-        { "element": "img", "class": "post-photo", "imageContainer": "figure.article-photo" },
-    ];
-
-    postStructure.forEach((elObj) => {
-
-        const createElement = document.createElement(elObj.element);
-
-        let imageContainer = null;
-
-        createElement.classList.add(elObj.class);
-
-        switch (elObj.class) {
-            case "insta-post":
-                num = getRandomInt(1, 11);
-                createElement.classList.add(`gradient${num}`);
-                createElement.classList.add(`post${postObj.id}`);
-                break;
-            case "post-photo":
-                createElement.setAttribute("src", postObj.image_url);
-                break;
-        }
-
-        imageContainer = document.querySelector(elObj.imageContainer);
-        imageContainer.appendChild(createElement);
-
-    });
-}
-
-createRow();
-
-createPostElement({
-    id: 1,
-    frame: "gradient001",
-    username: "soybeanboy",
-    message: "It's ruff out here for a puppy.",
-    image_url: "Assets/adorable-animal-animal-photography-animal-portrait-594687.jpg",
-    like_count: 2,
-    comments: [{
-            message: "Dogs are my favorite people."
-        },
-        {
-            message: "This friendship is fur real."
-        }
-    ]
-}, );
-
-createPostElement({
-    id: 1,
-    frame: "gradient001",
-    username: "soybeanboy",
-    message: "It's ruff out here for a puppy.",
-    image_url: "Assets/adorable-animal-animal-photography-animal-portrait-594687.jpg",
-    like_count: 2,
-    comments: [{
-            message: "Dogs are my favorite people."
-        },
-        {
-            message: "This friendship is fur real."
-        }
-    ]
-}, );
-
-createPostElement({
-    id: 1,
-    frame: "gradient001",
-    username: "soybeanboy",
-    message: "It's ruff out here for a puppy.",
-    image_url: "Assets/adorable-animal-animal-photography-animal-portrait-594687.jpg",
-    like_count: 2,
-    comments: [{
-            message: "Dogs are my favorite people."
-        },
-        {
-            message: "This friendship is fur real."
-        }
-    ]
-}, );
-
-createRow();
-
-createPostElement({
-    id: 1,
-    frame: "gradient001",
-    username: "soybeanboy",
-    message: "It's ruff out here for a puppy.",
-    image_url: "Assets/adorable-animal-animal-photography-animal-portrait-594687.jpg",
-    like_count: 2,
-    comments: [{
-            message: "Dogs are my favorite people."
-        },
-        {
-            message: "This friendship is fur real."
-        }
-    ]
-}, );
-createPostElement({
-    id: 1,
-    frame: "gradient001",
-    username: "soybeanboy",
-    message: "It's ruff out here for a puppy.",
-    image_url: "Assets/adorable-animal-animal-photography-animal-portrait-594687.jpg",
-    like_count: 2,
-    comments: [{
-            message: "Dogs are my favorite people."
-        },
-        {
-            message: "This friendship is fur real."
-        }
-    ]
-}, );
-createPostElement({
-    id: 1,
-    frame: "gradient001",
-    username: "soybeanboy",
-    message: "It's ruff out here for a puppy.",
-    image_url: "Assets/adorable-animal-animal-photography-animal-portrait-594687.jpg",
-    like_count: 2,
-    comments: [{
-            message: "Dogs are my favorite people."
-        },
-        {
-            message: "This friendship is fur real."
-        }
-    ]
-}, );
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -143,16 +15,173 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
 
+const renderPost = (postObj, rowId) => {
 
-/* 
-const postsContainer = document.querySelector("");
-const postElement = createPostElement(posts[0]);
-postsContainer.appendChild(postElement);
- */
+    const postStructure = [
+        { "element": "article", "classList": ["insta-post", `post-${postObj.id}`, `gradient${postObj.frame}`], "parent": `section.posts-row.row-${rowId}` },
+        { "element": "div", "classList": ["article-header"], "attributes": [], "parent": `article.insta-post.post-${postObj.id}` },
+        { "element": "h2", "classList": ["username"], "innerHTML": postObj.username, "parent": `article.insta-post.post-${postObj.id} div.article-header` },
+        { "element": "div", "classList": ["article-action-icons"], "parent": `article.insta-post.post-${postObj.id} div.article-header` },
+        { "element": "i", "innerHTML": "favorite", "parent": `article.insta-post.post-${postObj.id} div.article-header div.article-action-icons` },
+        { "element": "i", "innerHTML": "comment", "parent": `article.insta-post.post-${postObj.id} div.article-header div.article-action-icons` },
+        { "element": "i", "innerHTML": "share", "parent": `article.insta-post.post-${postObj.id} div.article-header div.article-action-icons` },
+        { "element": "figure", "classList": ["article-photo"], "parent": `article.insta-post.post-${postObj.id}` },
+        {
+            "element": "img",
+            "classList": ["post-photo"],
+            "attributes": [
+                ["src", postObj.image_url]
+            ],
+            "parent": `article.insta-post.post-${postObj.id} figure.article-photo`
+        },
+        { "element": "figcaption", "classList": ["article-photo-caption"], "parent": `article.insta-post.post-${postObj.id}` },
+        { "element": "p", "innerHTML": postObj.message, "parent": `article.insta-post.post-${postObj.id} figcaption.article-photo-caption` },
+        { "element": "div", "classList": ["article-comments"], "parent": `article.insta-post.post-${postObj.id}` },
+        { "element": "ul", "comments": postObj.comments, "parent": `article.insta-post.post-${postObj.id} div.article-comments` },
+        { "element": "form", "classList": ["comment-form"], "parent": `article.insta-post.post-${postObj.id}` },
+        {
+            "element": "input",
+            "attributes": [
+                ["type", "text"],
+                ["name", "comment"],
+                ["placeholder", "Add a comment..."]
+            ],
+            "parent": `article.insta-post.post-${postObj.id} form.comment-form`
+        },
+        { "element": "button", "innerHTML": "Post", "parent": `article.insta-post.post-${postObj.id} form.comment-form` },
+    ];
 
-/* 
-const postImage = document.createElement('img');
-postImage.classList.add("post-image");
-postImage.setAttribute("src", post.image_url);
-imageContainer.appendChild(postImage);
- */
+    postStructure.forEach((elObj) => {
+
+        const childEl = document.createElement(elObj.element);
+        const parentEl = document.querySelector(elObj.parent);
+
+        if (elObj.classList) {
+            elObj.classList.forEach(classItem => childEl.classList.add(classItem));
+        }
+
+        if (elObj.attributes) {
+            elObj.attributes.forEach(atr => childEl.setAttribute(atr[0], atr[1]));
+        }
+
+        if (elObj.innerHTML) {
+            childEl.innerHTML = elObj.innerHTML;
+        }
+
+        if (elObj.comments) {
+            elObj.comments.forEach((comment) => {
+                const listItem = document.createElement("LI");
+                listItem.innerHTML = `<i>account_circle</i>${comment.message}`;
+                childEl.appendChild(listItem);
+            });
+        }
+
+        parentEl.appendChild(childEl);
+
+    });
+
+}
+
+const renderContent = (postsArray) => {
+
+    let rowId = 1;
+
+    for (let i = 0; i < posts.length; i++) {
+
+        const postObj = postsArray[i];
+
+        if (i == 0) {
+            createRow(rowId);
+            renderPost(postObj, rowId);
+        } else if ((i - 1) % 3 == 0) {
+            renderPost(postObj, rowId);
+            rowId++;
+            createRow(rowId);
+        } else {
+            renderPost(postObj, rowId);
+        }
+
+
+
+    }
+
+}
+
+var posts = [{
+        id: 1,
+        frame: 1,
+        username: "soybeanboy",
+        message: "It's ruff out here for a puppy.",
+        image_url: "Assets/adorable-animal-animal-photography-animal-portrait-594687.jpg",
+        like_count: 2,
+        comments: [{
+                message: "Dogs are my favorite people."
+            },
+            {
+                message: "This friendship is fur real."
+            }
+        ]
+    },
+    {
+        id: 2,
+        frame: 11,
+        username: "soybeanboy",
+        message: "It's ruff out here for a puppy.",
+        image_url: "Assets/brown-and-white-short-coated-puppy-1805164.jpg",
+        like_count: 2,
+        comments: [{
+                message: "Dogs are my favorite people."
+            },
+            {
+                message: "This friendship is fur real."
+            }
+        ]
+    },
+    {
+        id: 3,
+        frame: 10,
+        username: "soybeanboy",
+        message: "It's ruff out here for a puppy.",
+        image_url: "Assets/high-angle-photo-of-a-corgi-looking-upwards-2664417.jpg",
+        like_count: 2,
+        comments: [{
+                message: "Dogs are my favorite people."
+            },
+            {
+                message: "This friendship is fur real."
+            }
+        ]
+    },
+    {
+        id: 4,
+        frame: 7,
+        username: "soybeanboy",
+        message: "It's ruff out here for a puppy.",
+        image_url: "Assets/pomeranian-puppy-1619690.jpg",
+        like_count: 2,
+        comments: [{
+                message: "Dogs are my favorite people."
+            },
+            {
+                message: "This friendship is fur real."
+            }
+        ]
+    },
+    {
+        id: 5,
+        frame: 5,
+        username: "soybeanboy",
+        message: "It's ruff out here for a puppy.",
+        image_url: "Assets/short-coated-white-dog-on-white-textile-846083.jpg",
+        like_count: 2,
+        comments: [{
+                message: "Dogs are my favorite people."
+            },
+            {
+                message: "This friendship is fur real."
+            }
+        ]
+    },
+]
+
+renderContent(posts);
